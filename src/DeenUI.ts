91e59a27e2c627b6c896331135daa8594842ffce
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2022-11-15 22:34:44 +0600

    Restructuring folders

--- src/DeenUI.ts
+++ src/DeenUI.ts
@@ -1,13 +1,29 @@
 import type {App} from 'vue'
-import {DRow} from "@/components/row-col/DRow";
-import {DCol} from "@/components/row-col/DCol";
+import {DRow} from "@/components/DRow/index";
+import {DCol} from "@/components/DCol/index";
 
-export default {
-  install: (app: App) => {
-    app.component('DRow', DRow)
-    app.component('DCol', DCol)
-  }
+const components = [
+  DRow,
+  DCol
+]
+
+const install = (app: App) => {
+  components.forEach(component => {
+    app.component(component, component);
+  });
+  // app.component('DRow', DRow)
+  // app.component('DCol', DCol)
 }
 
-export {DRow}
-export {DCol}
+if (typeof window !== 'undefined' && window?.Vue) {
+  install(window?.Vue);
+}
+
+export default {
+  version: '2.15.11',
+  // locale: locale.use,
+  // i18n: locale.i18n,
+  install,
+  DRow,
+  DCol
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-  import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+import typescript2 from "rollup-plugin-typescript2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    typescript2({
+      check: false,
+      include: []
+    })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
